dc4bf272b06244ba71aeed418f8ce8d9569479bc
sanhuazhang <sanhuazhang@tencent.com>
2017-03-23 11:09:36 +0800

    [sqlcipher]add info plist

---
+++ sqlcipher framework/sqcliphertest.h
@@ -0,0 +1,19 @@
+//
+//  sqcliphertest.h
+//  sqcliphertest
+//
+//  Created by sanhuazhang on 2017/3/23.
+//  Copyright © 2017年 sanhuazhang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for sqcliphertest.
+FOUNDATION_EXPORT double sqcliphertestVersionNumber;
+
+//! Project version string for sqcliphertest.
+FOUNDATION_EXPORT const unsigned char sqcliphertestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sqcliphertest/PublicHeader.h>
+
+
